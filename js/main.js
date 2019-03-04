9fe0209624d0c3fab7f1aed519bde5744670b0b9
Vira <vira.romanko99@gmail.com>
2019-03-04 13:20:10 -0500

    transitions

--- js/main.js
+++ js/main.js
@@ -17,7 +17,31 @@
 		// play the audio tag
 		audio.currentTime = 0;
 		audio.play();
+
+
+		// select the parent div and animate it
+		// rty selecting the matching parent div element
+		let key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+		key.classList.add("playing");
+	}
+
+	function removePlayingClass(e) {
+		// listen  for a CSS  transition to finish the reste the element
+		//by removing the playing class
+		//debugger;
+
+		if (e.propertyName !== "transform"){
+			return;
+		} else{
+			console.log("do some stuff here.transition is done");
+			e.currentTarget.classList.remove("playing");
+		}
 	}
 
 	window.addEventListener("keydown", playDrumSound);
+	// grab all of the keyboard keys that have the .key class applied
+	const keys = Array.from(document.querySelectorAll(".key"));
+
+	//add a transition end to each key 
+	keys.forEach(key => key.addEventListener("transitionend", removePlayingClass));
 })();
